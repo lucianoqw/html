085e65c044760b40fc0fb0070b376f465fb10ee1
lucianoqw <112838603+lucianoqw@users.noreply.github.com>
2022-09-14 22:03:52 -0300

    initial commit

---
+++ estudandojavascript/comentarios.js
@@ -0,0 +1,67 @@
+//aqui eu tenho comentarios em linha
+/*
+aqui eu tenho comentariomulti linha 
+o codigo abaixo escreve alguma mensagem no davtools */
+console.log('bem vindo ao starter') 
+
+/*cadeia de caracteres 
+"" //aspas duplas
+''// aspas simples 
+``// template literals ou template strings
+
+number
+  * números
+ 33// inteiros 
+ 12,5// reais-floot
+ NaN// Not a number
+ Infinity// infinito
+      
+  boolean
+   * somente 2 valores 
+  true// verdadeiro 
+  false// falso
+
+  undefined
+   *indefinido
+ null
+  *nulo 
+  *objeto que nao possui nada dentro 
+
+
+  Object
+    *objeto
+    *propriedades/atributos
+    *funcionalidades / métodos
+    {propriedade: "valor"}
+
+    array( vetores)
+    * uma lista 
+    *agrupamento de dado
+    ["luciano", 36]*/
+
+
+ /* 
+  #tipos de dados
+  conforme o ecmascript standard temos 9 tipos dedados:
+  * data types
+   primitive/ primitive value
+   * strutural
+   * strutural primitive
+# primitivos
+ * string
+ * number
+ * boolean
+ * undeined
+ * symbol
+ * bigtnt
+#Estruturais
+ * object
+  * Array
+  * map
+  * set
+  * date
+  * ...
+# function
+# primitivo estutural / strutural root primitive
+* null */ 
+ 
---
+++ estudandojavascript/script.js
@@ -0,0 +1,17 @@
+console.log('bem vindo ao starter') 
+console.log({
+    name: "luciano",
+    idade: 36, 
+    andar: function(){
+        console.log('andar')
+    }
+
+})
+
+
+console.log([
+"leite", 
+"ovos",
+2,
+3
+])
